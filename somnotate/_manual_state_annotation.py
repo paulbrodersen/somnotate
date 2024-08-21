7c78aa6c8f0bf86242ce57b099b89f55f4acceb7
Paul Brodersen <paulbrodersen@gmail.com>
2024-08-21 12:56:17 +0100

    Bugfix: fix TimeSeriesViewer._update_selection (addresses issue #16)
    `matplotlib.axes.axvspan`'s `get_xy` and `set_xy` no longer retrieve
    and set the list of rectangle vertices, but only its origin.

--- somnotate/_manual_state_annotation.py
+++ somnotate/_manual_state_annotation.py
@@ -588,20 +588,9 @@ class TimeSeriesStateViewer(object):
 
 
     def _update_selection(self, selection_lower_bound, selection_upper_bound):
-        vertices = self.rect.get_xy()
-        # vertices are returned in the following order
-        #     - lower-left
-        #     - upper-left
-        #     - upper-right
-        #     - lower-right
-        #     - lower-left
-
-        # set new x-values for vertices
-        vertices[[0, 1, -1], 0] = selection_lower_bound
-        vertices[[2, 3],     0] = selection_upper_bound
-
         # update rectangle coordinates and epoch bounds
-        self.rect.set_xy(vertices)
+        self.rect.set_xy((selection_lower_bound, 0))
+        self.rect.set_width(selection_upper_bound - selection_lower_bound)
         self.selection_lower_bound = selection_lower_bound
         self.selection_upper_bound = selection_upper_bound
         self.figure.canvas.draw_idle()
