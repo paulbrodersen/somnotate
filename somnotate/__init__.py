8714b17e38c469b5836f9e19577fb3643b1fd8fd
Paul Brodersen <paulbrodersen@gmail.com>
2024-11-12 13:37:01 +0000

    Update example spreadsheet

--- somnotate/__init__.py
+++ somnotate/__init__.py
@@ -20,6 +20,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "0.3.6"
+__version__ = "0.4.0"
 __author__ = "Paul Brodersen"
 __email__ = "paulbrodersen+somnotate@gmail.com"
